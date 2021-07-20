50cba97dd8de477cd5c6588361ccf4db0439ad81
Kurt Kaiser <kurtbkaiser@gmail.com>
2021-07-19 22:44:08 -0400

    Working running animation
    Video tutorial recorded for this part

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,24 +5,45 @@ using UnityEngine.AI;
 
 public class Movement : MonoBehaviour
 {
+    NavMeshAgent navAgent;
+
+    private void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+    }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             MoveToCursor();
-        }        
+        }
+        UpdateAnimator();
     }
 
 
     private void MoveToCursor()
     {
+        navAgent.speed = Input.GetKey(KeyCode.LeftShift) ? 6 : 4.5f;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         bool hasHit = Physics.Raycast(ray, out hit);
         if (hasHit)
         {
-            GetComponent<NavMeshAgent>().destination = hit.point;
+            navAgent.destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        float speed = 0;
+        if(navAgent.remainingDistance > 0)
+        {
+            Vector3 velocity = navAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            speed = localVelocity.z;
+        }
+        GetComponent<Animator>().SetFloat("forwardMotion", speed);
+
     }
 }
